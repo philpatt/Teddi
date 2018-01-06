8b21180bc45c829edaa1dc5d5b863b6db6f98ec3
philpatt <phillip.s.patterson1@gmail.com>
2018-01-05 16:34:10 -0800

    initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var passport = require('../config/passportConfig');
+var db = require('../models');
+var router = express.Router();
+
+
+//get login form
+router.get('/login', function(req, res){
+	res.render('auth/login');
+});
+//post login info on form 
+router.post('/login', passport.authenticate('local', {
+	successRedirect: '/profile', 
+	successFlash: 'Login Successful!',
+	failureRedirect: '/auth/login',
+	failureFlash : 'Invalid Credentials'
+}));
+//get sign up form
+router.get('/signup', function(req, res, next){
+	res.render('auth/signup');
+});
+// post signup info on form
+router.post('/signup', function(req, res, next){
+	console.log('req.body is', req.body.email);
+	db.user.findOrCreate({
+		where: { email: req.body.email },
+		defaults: {
+			username: req.body.username,
+			firstname: req.body.firstname,
+			lastname: req.body.lastname,
+			password: req.body.password
+		}
+	}).spread(function(user, wasCreated){
+		if(wasCreated){
+			//goodjob, you didnt make a duplicate!
+			passport.authenticate('local', {
+				successRedirect: '/profile',
+				successFlash: 'successfuly logged in'
+			})(req, res, next);
+		}
+		else{
+			//badnews you tried to sign up when you should have logged in
+			req.flash('error', 'email already exists');
+			res.redirect('/auth/login');
+		}
+	}).catch(function(err){
+		req.flash('error', error.message)
+		res.redirect('/auth/signup');
+	});
+});
+
+// logout
+router.get('/logout', function(req, res){
+	req.logout();
+	req.flash('success', 'successfuly logged out');
+	res.redirect('/');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,44 @@
+// define all requires/modules/packages
+
+require('dotenv').config();
+var bodyParser = require('body-parser');
+var express = require('express');
+var ejsLayouts = require('express-ejs-layouts');
+var flash = require('connect-flash');
+var isLoggedIn = require('./middleware/isloggedin')
+var passport = require('./config/passportConfig');
+var session = require('express-session');
+var app = express ();
+
+//define middleware
+
+app.set('view engine', 'ejs');
+app.set(bodyParser.urlencoded({ extended: false}));
+app.use(ejsLayouts);
+app.use(session({
+	secret:'abc123',
+	resave: false,
+	saveUninitialized: true
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req, res, next){
+	res.locals.currentUser = req.user;
+	res.locals.alerts = req.flash();
+	next();
+});
+
+//routes
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.get('/profile', isLoggedIn, function(req, res){
+	res.render('profile');
+});
+
+//define controllers
+app.use('/auth', require('./controllers/auth'));
+
+app.listen(process.env.PORT || 3000);
