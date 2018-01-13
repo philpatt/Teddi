420b1518e552d7635bc0e91c1cdd9756c9af20c6
philpatt <phillip.s.patterson1@gmail.com>
2018-01-12 21:57:57 -0800

    begin styling

--- controllers/search.js
+++ controllers/search.js
@@ -10,25 +10,28 @@ var request = require('request');
 
 
 // show page that is results of search
-router.get('/', isLoggedIn, function(req, res){
+router.get('/', function(req, res){
 	var qs ={
 		start: 1,
 		q: req.query.q || 'Rainier',
 		api_key: process.env.API_KEY,
 		sort: 'name',
-		fields: 'images,entranceFees'
+		fields: 'entranceFees'
 	}
 	request({
 	    url: 'https://developer.nps.gov/api/v1/parks',
 	    qs: qs
 	  }, function (error, response, body) {
-  	// console.log(body);
-    if (!error && response.statusCode == 200) {
-      var dataObj = JSON.parse(body).data;
-      console.log('this is my data', dataObj);
-      res.render('results',{ results: dataObj});
-    }
-  });
+	  	if (!currentUser && !error && response.statusCode == 200) {
+      		var dataObj = JSON.parse(body).data;
+      		console.log('this is my data', dataObj);
+      		res.render('noProf',{ results: dataObj});
+		} else if (currentUser && !error && response.statusCode == 200 ) {}{
+			var dataObj = JSON.parse(body).data;
+			console.log('this is my data', dataObj);
+			res.render('results',{ results: dataObj});
+		}
+	});
 });
 
 
