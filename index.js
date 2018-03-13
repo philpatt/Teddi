bd5e461259a2e25cddb0ea52693a19ae5586d362
philpatt <phillip.s.patterson1@gmail.com>
2018-03-13 13:28:40 -0700

    added secret/apikey to teddi heroku account

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(ejsLayouts);
 app.use(session({
-	secret: 'abc123',
+	secret: process.env.SESSION_SECRET,
 	resave: false,
 	saveUninitialized: true
 }));
